af472b558a0d8139b593c570e064557016ee424e
Formid <patrick322@naver.com>
2024-01-10 18:12:05 +0900

    이름 누르면 이동하기까지 구현 완료

--- src/index.tsx
+++ src/index.tsx
@@ -1,76 +1,146 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import styled from "@emotion/styled";
+import { useHorizontalScroll } from "./useSideScroll.tsx";
 
-const Header = styled.div`
+const App = () => {
+  const scrollRef = useHorizontalScroll();
+
+  const scrollToPosition = (positionX : number) => {
+    if(scrollRef.current) {
+      scrollRef.current.scrollTo({
+        left: positionX,
+        behavior: 'smooth'
+      })
+    }
+  }
+
+  return (
+    <>
+    <NavigationBar f={scrollToPosition}></NavigationBar>
+    <div className="App" ref={scrollRef} style={{ overflow: "auto" }}>
+      <div style={{ whiteSpace: "nowrap" }}>
+        <StoryBox f={scrollToPosition}></StoryBox>
+      </div>
+    </div></>
+  );
+
+}
+
+const StoryBoxRoot = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  height: 50vw;
+  width: 500vw;
+  gap: 240px;
+  padding-left: 36px;
+  
+  background: #EED555;
+`
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: row;
+  // border: 1px solid black;
+`
+
+const Story = styled.div`
+  display: flex;
+  word-wrap: break-word;
+  white-space: normal;
+
+  margin-top: 240px;
+  margin-left: 12px;
+  width: 480px;
+  height: 200px;
+  font-size: 20px;
+  // border: 1px solid black;
+`
+
+const Name = styled.span`
+  color: blue;
+  cursor: pointer;
+`
+
+
+interface StoryBoxProps {
+  f : (positionX : number) => void
+}
+const StoryBox = ({f} : StoryBoxProps) => {
+
+  return (
+    <StoryBoxRoot>
+      
+    <Box>
+      <Story>
+        <p>
+        <Name onClick={() => f(550)}>양규</Name>의 아들, 한석이 고종 병술년 11월 28일에 태어난다. 이후 <Name onClick={() => f(1000)}>한석</Name>은 예규에게 입양된다. 한석은 보성 선씨와 결혼한다. 배우자인 선씨는 신묘년 2월 15일에 태어났으며, 아버지의 성함은 선영문이다. 한석은 1959년 5월 4일에 생을 마감하고, 가장등 가족묘지에 잠들어있다. 선씨는 1958년 12월 20일에 생을 마감하고, 우동간 합봉유비에 잠들어있다.</p>
+      </Story>
+    </Box>
+    <Box>
+      <Story>
+      한석과 선씨의 첫째 아들, 락천이 1913년 정월 11일에 태어난다. 락천은 장흥 임씨와 결혼한다. 배우자인 임씨는 1912년 8월 31일에 태어났으며, 아버지의 성함은 임흥태이다. 락천은 1990년 7월 27일에 생을 마감하고, 가장등 가족묘지에 잠들어있다.
+      </Story>
+      <Story>
+      한석과 선씨의 둘째 아들, 락재가 1917년 정월 10일에 태어난다. 락재는 경주 김씨 안순과 결혼한다. 배우자인 안순은 1923년 8월 27일에 태어났으며, 아버지의 성함은 김윤호이다. 락재는 1949년 5월 13일에 생을 마감하고, 탑동에 잠들어있다.
+      </Story>
+      <Story>
+      한석과 선씨의 셋째 아들, 락행이 1926년 2월 18일에 태어난다. 락행은 달성 배씨 갑자와 결혼한다. 배우자인 갑자는 1924년 9월 5일에 태어났으며, 아버지의 성함은 배평묵이다. 락행은 1959년 8월 7일에 생을 마감하고, 탑동에 잠들어있다.
+      </Story>
+    </Box>
+    <Box>
+        <Story>
+        </Story>
+    </Box>
+    <Box>
+    </Box>
+    <Box>
+      <Story>
+      </Story>
+    </Box>
+      
+    </StoryBoxRoot>
+  )
+}
+
+
+const NavigationBarRoot = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  height : 40px;
+  width : 90vw;
+  position: fixed;
+  top: 0;
+  z-index: 100;
+  margin: 48px;
+`
 
-  //   background-color: blue;
-  //   margin-bottom: 100px;
-  height: 50px;
-  width: 100%;
-  border: 1px solid black;
-`;
-
-const Body = styled.div`
+const NavigationBarItem = styled.div`
   display: flex;
-  width: 100%;
-`;
-
-const ItemBox = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  height: 500px;
-  border: 1px solid black;
-`;
-
-interface ItemProps {
-  generation: number;
-  people: string[];
+  font-size: 32px;
+`
+interface NavigationBarProps {
+  f : (positionX : number) => void
+}
+const NavigationBar = ({f} : NavigationBarProps) => {
+  return (
+    <NavigationBarRoot>
+      <NavigationBarItem onClick={() => f(0)}>[청주 김씨]</NavigationBarItem>
+      <NavigationBarItem onClick={() => f(1000)}>15대</NavigationBarItem>
+      <NavigationBarItem onClick={() => f(2000)}>16대</NavigationBarItem>
+      <NavigationBarItem onClick={() => f(3000)}>17대</NavigationBarItem>
+      <NavigationBarItem onClick={() => f(4000)}>18대</NavigationBarItem>
+      <NavigationBarItem onClick={() => f(5000)}>19대</NavigationBarItem>
+    </NavigationBarRoot>
+  )
 }
 
-const Item = ({ generation, people }: ItemProps) => {
-  const [onMouse, setOnMouse] = useState<boolean>(false);
-  const handleMouseEnter = () => {
-    setOnMouse(true);
-  };
-  const handleMouseLeave = () => {
-    setOnMouse(false);
-  };
-
-  return (
-    <ItemBox onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {onMouse ? (
-        people.map((person, index) => (
-          <div key={index}>{onMouse ? `${person}` : `${generation}대`}</div>
-        ))
-      ) : (
-        <div>{generation}대</div>
-      )}
-    </ItemBox>
-  );
-};
 
 ReactDOM.render(
-  <>
-    <Header>
-      <div>청주김씨의 일원이신가요?</div>
-      <div>[청주김씨]</div>
-      <div>전할 이야기가 있나요?</div>
-    </Header>
-    <Body>
-      <Item generation={15} people={["김선형", "강혁준"]} />
-      <Item generation={16} people={["김마크"]} />
-      <Item generation={17} people={["김또또", "강성민"]} />
-      <Item generation={18} people={["송호준", "연식"]} />
-      <Item generation={19} people={["김한별", "구경회"]} />
-    </Body>
-  </>,
+  <App/>
+ ,
   document.getElementById("root")
 );
-console.log("Hello, world!");
---
+++ src/useSideScroll.tsx
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from "react";
+
+export function useHorizontalScroll( ): React.RefObject<HTMLDivElement> {
+  const elRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const el = elRef.current;
+    if (el) {
+      const onWheel = e => {
+        if (e.deltaY == 0) return;
+        e.preventDefault();
+        el.scrollTo({
+          left: el.scrollLeft + 5*e.deltaY,
+          behavior: "smooth"
+        });
+      };
+      el.addEventListener("wheel", onWheel);
+      return () => el.removeEventListener("wheel", onWheel);
+    }
+  }, []);
+  return elRef;
+}
